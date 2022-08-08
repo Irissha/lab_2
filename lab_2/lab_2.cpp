c9a9c35c76cfea7104d4e53d8e99ed8d91981a38
Ирина <@DESKTOP-MSGGBKL>
2022-08-09 00:21:39 +0500

    Добавьте файлы проекта.

---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int sum = 0;
+	for (int i = 1; i < 100; i++) {
+		if (i % 2 == 0) {
+			sum = sum + i;
+		}
+	}
+	cout << sum;
+	return 0;
+}
